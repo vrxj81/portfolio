de981fab22e613594c82b4db936afce5ac4799b8
Johan Vrolix <johan@vrolix.eu>
2025-02-28 13:49:06 +0100

    feat(jobboard-backend): added Initial Seed data

---
+++ apps/jobboard/backend/src/migrations/1740746786107-InitSeed.ts
@@ -0,0 +1,19 @@
+import { IRole } from '@portfolio/common-models';
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitSeed1740746786107 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const roleRepository = queryRunner.manager.getRepository<IRole>('Role');
+
+    await roleRepository.save(
+      await roleRepository.create({
+        name: 'recruiter',
+        description: 'Recruiter role',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager.getRepository('Role').delete({});
+  }
+}
