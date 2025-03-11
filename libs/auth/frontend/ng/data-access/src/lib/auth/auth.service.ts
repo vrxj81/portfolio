91a706e9f302e628fef02e2251e52802d9ad82bd
Johan Vrolix <johan@vrolix.eu>
2025-03-11 16:14:45 +0100

    feat(auth-frontend-ng-data-access): added error handler usage

--- libs/auth/frontend/ng/data-access/src/lib/auth/auth.service.ts
+++ libs/auth/frontend/ng/data-access/src/lib/auth/auth.service.ts
@@ -5,6 +5,8 @@ import {
   LoginRequestDto,
   RegisterRequestDto,
 } from '@portfolio/common-dtos';
+import { catchError, throwError } from 'rxjs';
+import { handleHttpError } from '@portfolio/frontend-util-error-handler';
 
 @Injectable({
   providedIn: 'root',
@@ -14,34 +16,56 @@ export class AuthService {
   private apiUrl = '/api/auth';
 
   login(data: LoginRequestDto) {
-    return this.http.post<AuthResponseDto>(`${this.apiUrl}/login`, data);
+    return this.http.post<AuthResponseDto>(`${this.apiUrl}/login`, data).pipe(
+      catchError((error) => {
+        return throwError(() => handleHttpError(error));
+      }),
+    );
   }
 
   register(data: RegisterRequestDto) {
-    return this.http.post<AuthResponseDto | { registered: boolean }>(
-      `${this.apiUrl}/register`,
-      data,
-    );
+    return this.http
+      .post<
+        AuthResponseDto | { registered: boolean }
+      >(`${this.apiUrl}/register`, data)
+      .pipe(
+        catchError((error) => {
+          return throwError(() => handleHttpError(error));
+        }),
+      );
   }
 
   activate(userId: string, token: string) {
-    return this.http.patch<{ activated: boolean }>(
-      `${this.apiUrl}/activate/${userId}`,
-      { token },
-    );
+    return this.http
+      .patch<{
+        activated: boolean;
+      }>(`${this.apiUrl}/activate/${userId}`, { token })
+      .pipe(
+        catchError((error) => {
+          return throwError(() => handleHttpError(error));
+        }),
+      );
   }
 
   forgotPassword(email: string) {
-    return this.http.post<{ forgot: boolean }>(
-      `${this.apiUrl}/forgot-password`,
-      email,
-    );
+    return this.http
+      .post<{ forgot: boolean }>(`${this.apiUrl}/forgot-password`, email)
+      .pipe(
+        catchError((error) => {
+          return throwError(() => handleHttpError(error));
+        }),
+      );
   }
 
   resetPassword(token: string, password: string) {
-    return this.http.patch<{ reset: boolean }>(
-      `${this.apiUrl}/reset-password/${token}`,
-      { password },
-    );
+    return this.http
+      .patch<{
+        reset: boolean;
+      }>(`${this.apiUrl}/reset-password/${token}`, { password })
+      .pipe(
+        catchError((error) => {
+          return throwError(() => handleHttpError(error));
+        }),
+      );
   }
 }
