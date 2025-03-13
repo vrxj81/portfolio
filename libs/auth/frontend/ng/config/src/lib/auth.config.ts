05301128e81857d60afaab4251ed9c616d5c7560
Johan Vrolix <johan@vrolix.eu>
2025-03-13 20:51:25 +0100

    init(auth-frontend-ng-config): Initial setup of frontend auth config

---
+++ libs/auth/frontend/ng/config/src/lib/auth.config.ts
@@ -0,0 +1,14 @@
+import { InjectionToken } from "@angular/core";
+
+export interface AuthConfig {
+  loginRedirectUrl: string;
+  logoutRedirectUrl: string;
+}
+
+export const AUTH_CONFIG = new InjectionToken<AuthConfig>('auth.config', {
+  providedIn: 'root',
+  factory: () => ({
+    loginRedirectUrl: '/',
+    logoutRedirectUrl: '/',
+  }),
+});
