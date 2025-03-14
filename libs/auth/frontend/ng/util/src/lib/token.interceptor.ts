7ad6a4529747ccdb643189f44714cdce5cb8e5c8
Johan Vrolix <johan@vrolix.eu>
2025-03-14 11:21:18 +0100

    feat(auth-frontend-ng-util): added refresh token handling to token interceptor

--- libs/auth/frontend/ng/util/src/lib/token.interceptor.ts
+++ libs/auth/frontend/ng/util/src/lib/token.interceptor.ts
@@ -1,6 +1,7 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { effect, inject } from '@angular/core';
 import { AuthStore } from '@portfolio/auth-frontend-ng-state';
+import { catchError, throwError } from 'rxjs';
 
 export const TokenInterceptor: HttpInterceptorFn = (req, next) => {
   const authStore = inject(AuthStore);
@@ -11,5 +12,31 @@ export const TokenInterceptor: HttpInterceptorFn = (req, next) => {
       },
     });
   }
-  return next(req);
+  return next(req).pipe(
+    catchError((error) => {
+      if (
+        error instanceof HttpErrorResponse &&
+        error.status === 401 &&
+        localStorage.getItem('refreshToken')
+      ) {
+        authStore.refreshToken(localStorage.getItem('refreshToken') || '');
+        effect(() => {
+          if (authStore.isRegistered()) {
+            req = req.clone({
+              setHeaders: {
+                Authorization: `Bearer ${authStore.accessToken()}`,
+              },
+            });
+            return next(req);
+          }
+          if (authStore.error()) {
+            return throwError(() => authStore.error());
+          }
+          return throwError(() => error);
+        });
+        return next(req);
+      }
+      return throwError(() => error);
+    }),
+  );
 };
