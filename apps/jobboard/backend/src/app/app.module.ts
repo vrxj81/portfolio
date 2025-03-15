dc90e437ccbad8b984ce64a73ea8eeb1fa84a363
Johan Vrolix <johan@vrolix.eu>
2025-03-15 10:36:12 +0100

    feat(jobboad-backend): added node mailermodule and auth mailer module

--- apps/jobboard/backend/src/app/app.module.ts
+++ apps/jobboard/backend/src/app/app.module.ts
@@ -3,17 +3,24 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { authConfig, jwtConfig } from '@portfolio/auth-backend-config';
-import { DbConfig, dbConfig } from '@portfolio/backend-config';
+import {
+  DbConfig,
+  dbConfig,
+  MailConfig,
+  mailConfig,
+} from '@portfolio/backend-config';
 import { AuthBackendInterfaceModule } from '@portfolio/auth-backend-interface';
 import { User } from '@portfolio/backend-data-access-users';
 import { Role } from '@portfolio/backend-data-access-roles';
 import { Permission } from '@portfolio/backend-data-access-permissions';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { AuthBackendMailerModule } from '@portfolio/auth-backend-mailer';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [jwtConfig, authConfig, dbConfig],
+      load: [jwtConfig, authConfig, dbConfig, mailConfig],
     }),
     TypeOrmModule.forRootAsync({
       inject: [dbConfig.KEY],
@@ -32,7 +39,28 @@ import { Permission } from '@portfolio/backend-data-access-permissions';
       }),
     }),
     EventEmitterModule.forRoot(),
+    MailerModule.forRootAsync({
+      inject: [mailConfig.KEY],
+      useFactory: (mailConfig: MailConfig) => ({
+        transport: {
+          host: mailConfig.host,
+          port: mailConfig.port,
+          secure: mailConfig.secure,
+          auth: {
+            user: mailConfig.user,
+            pass: mailConfig.pass,
+          },
+        },
+        defaults: {
+          from: mailConfig.default,
+          to: mailConfig.default,
+        },
+        ignoreTLS: mailConfig.ignoreTls,
+        rejectUnauthorized: mailConfig.rejectUnauthorized,
+      }),
+    }),
     AuthBackendInterfaceModule,
+    AuthBackendMailerModule,
   ],
   controllers: [],
   providers: [],
