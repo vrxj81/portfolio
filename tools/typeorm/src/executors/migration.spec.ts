ffb4fb6c15779659c6697e3ddf8d4dc665565b54
Johan Vrolix <johan@vrolix.eu>
2025-03-16 22:13:15 +0100

    chore(@portfolio/source): lint dependencies for migration of NX

--- tools/typeorm/src/executors/migration.spec.ts
+++ tools/typeorm/src/executors/migration.spec.ts
@@ -3,8 +3,16 @@ import { ExecutorContext } from '@nx/devkit';
 import { TypeormMigrationExecutorSchema } from './schema';
 import executor from './migration';
 
-const options: TypeormMigrationExecutorSchema = {};
+jest.mock('child_process', () => ({
+  execSync: jest.fn(),
+}));
+
+const options: TypeormMigrationExecutorSchema = {
+  command: 'create',
+  name: 'migration',
+};
 const context: ExecutorContext = {
+  projectName: 'my-app',
   root: '',
   cwd: process.cwd(),
   isVerbose: false,
@@ -13,7 +21,14 @@ const context: ExecutorContext = {
     dependencies: {},
   },
   projectsConfigurations: {
-    projects: {},
+    projects: {
+      'my-app': {
+        root: 'apps/my-app',
+        sourceRoot: 'apps/my-app/src',
+        projectType: 'application',
+        targets: {},
+      },
+    },
     version: 2,
   },
   nxJsonConfiguration: {},
